063d6c5f03e160eebdccbc56a7a8271d9030bb84
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-04-18 19:06:30 -0300

    foreach e arrow function

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
-const animais = document.getElementById('animais')
-console.log(animais)
+// foreach e arrow function 
+const imagens = document.querySelectorAll('img')
 
-const gridSection = document.getElementsByClassName('grid-section')
-console.log(gridSection[2])
+// imagens.forEach(function(imagem){
+//   console.log(imagem)
+// })
 
-const primeiraLI = document.querySelector('li')
-console.log(primeiraLI)
+const titulos = document.getElementsByClassName('titulo')
+const titulosArray = Array.from(titulos)
 
-const linkInterno = document.querySelector('[href^="#"]')
-console.log(linkInterno.href)
+// titulosArray.forEach((titulo) => console.log(titulo))
 
-const linksInterno = document.querySelectorAll('[href^="#"]')
-console.log(linksInterno)
-
-const animaisImg = document.querySelectorAll('.animais img')
-console.log(animaisImg)
-
-const primeiraUl = document.querySelector('ul')
-console.log(primeiraUl)
-
-const gridSectionHtml = document.getElementsByClassName('grid-section')
-const gridSectionNode = document.querySelectorAll('.grid-section')
-
-primeiraUl.classList.add('grid-section')
-
-// console.log(gridSectionHtml[0])
-// console.log(gridSectionNode[0])
-
-gridSectionNode.forEach(function(item, index){
-  console.log(item)
-})
-
-const arrayGrid = Array.from(gridSectionHtml)
-
-// Exercicios 
-// 1 - Retorne no console todas as imagens do site
-const todasAsImagens = document.querySelectorAll('img')
-console.log(todasAsImagens)
-
-// 2 - Retorne no console apenas as imagens que começaram com a palavra imagem
-const imagens = document.querySelectorAll('[src^="./img/imagem"]')
-console.log(imagens)
-
-// 3 - Selecione todos os links internos onde o href começa com  #
-const link = document.querySelectorAll('[href^="#"]')
-console.log(link)
-
-// 4 - Selecione o primeiro h2  dentro de .animais-descricao
-const primeiroH2 = document.querySelector('.animais-descricao h2')
-console.log(primeiroH2)
-
-// 5 - Selecione o ultimo p do site
+// Mostre no console cada parágrado do site
 const paragrafos = document.querySelectorAll('p')
-console.log(paragrafos[paragrafos.length - 1])
+
+paragrafos.forEach((paragrafo) => console.log(paragrafo.innerText))
+
+// Mostre o texto dos parágrafos no console
+
+
+// Como corrigir os erros abaixo:
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+// let i = 0;
+// imgs.forEach(() => {
+//   console.log(i++);
+// });
+
+// imgs.forEach(() => i++);
+
