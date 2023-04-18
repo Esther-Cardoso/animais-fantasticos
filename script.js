a13d14860478a66738ec14a5508ba228adaefc36
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-04-17 21:08:47 -0300

    seleção de elementos

---
+++ script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais')
+console.log(animais)
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection[2])
+
+const primeiraLI = document.querySelector('li')
+console.log(primeiraLI)
+
+const linkInterno = document.querySelector('[href^="#"]')
+console.log(linkInterno.href)
+
+const linksInterno = document.querySelectorAll('[href^="#"]')
+console.log(linksInterno)
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg)
