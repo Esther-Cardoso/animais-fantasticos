72bd5390adcbfd27516e4a3ef5ace4c942017ab0
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-04-29 10:47:06 -0300

    Dimensões e distâncias exercicios

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-// Dimensões e distâncias
-const listaAnimais = document.querySelector('.animais-lista')
-const footer = document.querySelector('.copy')
+// Dimensões e distâncias 
+// Exercícios
 
-console.log(listaAnimais.clientHeight)
-console.log(footer.offsetTop)
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const primeiraImg = document.querySelector('img')
+console.log(`A imagem está ${primeiraImg.offsetTop}px do topo`)
 
-const primeiroH2 = document.querySelector('h2')
-const h2left = primeiroH2.offsetLeft
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const todasImagens = document.querySelectorAll('img')
 
-console.log(h2left)
+  let soma = 0
+  todasImagens.forEach((imagem) => {
+    soma += imagem.offsetWidth
+  })
+  console.log(`A soma da largura de todas as imagens é ${soma}`)
+}
 
-const rect = primeiroH2.getBoundingClientRect()
-console.log(rect)
-console.log(rect.height)
+window.onload = function() {
+  somaImagens()
+}
 
-console.log(
-  window.innerWidth,
-  window.innerHeight
-)
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a')
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth
+  const linkHeight = link.offsetHeight
+  if(linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, 'Possui boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibilidade')
+  }
+})
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+const mobile = window.matchMedia('(max-width: 720px)').matches
+
+if(mobile) {
+  const menu = document.querySelector('.menu')
+  menu.classList.add('menu-mobile')
+}
