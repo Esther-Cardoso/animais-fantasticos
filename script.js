301108242e8ff6fee18464f9fd73ba40e0abcf87
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-04-29 19:20:05 -0300

    exercicios eventos

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-// Eventos
-const img = document.querySelector('img')
+const imgs = document.querySelectorAll('img')
 
-function callback(event) {
-  console.log(event)
+function handleImg(event) {
+  console.log(event.target.getAttribute('src'))
 }
 
-img.addEventListener('click', callback)
+imgs.forEach((img) => {
+  img.addEventListener('click', handleImg)
+})
 
-const animaisLista = document.querySelector('.animais-lista')
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"]')
 
-function callLista(event) {
-  console.log(event.currentTarget)
-  console.log(event.target)
-  console.log(this)
-}
-
-animaisLista.addEventListener('click', callLista)
-
-const linkExterno = document.querySelector('a[href^="http"]')
-
-function handleLinkExterno(event) {
+function handleLink(event) {
   event.preventDefault()
-  console.log('Clicou')
-  console.log(this.getAttribute('href'))
+  linksInternos.forEach((link) => {
+    link.classList.remove('ativo')
+  })
+  this.classList.add('ativo')
 }
 
-linkExterno.addEventListener('click', handleLinkExterno)
+linksInternos.forEach((link) => {
+  link.addEventListener('click', handleLink)
+})
 
-const h1 = document.querySelector('h1')
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
 
-function handleEvent(event) {
-  console.log(event.type, event)
+const todosElementos = document.querySelector('body')
+
+function handleElementos(event) {
+  console.log(event.target)
+  // console.log(event.target.remove())
 }
 
-h1.addEventListener('click', handleEvent)
-// h1.addEventListener('mouseenter', handleEvent)
-// h1.addEventListener('mousemove', handleEvent)
+todosElementos.addEventListener('click', handleElementos)
 
-// window.addEventListener('scroll', handleEvent)
 
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
 function handleKeyboard(event) {
-  if(event.key === 'f') {
-    document.body.classList.toggle('fullscreen')
+  if(event.key === 't') {
+    document.body.style.fontSize = '24px'
   }
 }
 
