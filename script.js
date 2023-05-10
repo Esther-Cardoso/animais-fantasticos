95e0d5781f2dc4a0504071708a42fb764fa4eda3
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-05-09 23:03:04 -0300

    animação ao scroll

--- script.js
+++ script.js
@@ -76,3 +76,29 @@ function initTop() {
 }
 
 initTop()
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll')
+
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6
+  
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top
+        const isSectionVisible = (sectionTop - windowMetade) < 0
+        if(isSectionVisible) {
+          section.classList.add('ativo')
+        } else {
+          section.classList.remove('ativo')
+        }
+      })
+    }
+    
+    animaScroll()
+    
+    window.addEventListener('scroll', animaScroll)
+  }  
+}
+
+initAnimacaoScroll()
