dddf3fcc39bf3fb8566a399cfdbb965e1cc69684
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-05-24 10:46:41 -0300

    Exercicio Number e Math

--- objetos.js
+++ objetos.js
@@ -1,73 +1,49 @@
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+let valor = 20.85
 
-const transacoes = [
-  {
-    descricao: 'Taxa do Pão',
-    valor: 'R$ 39',
-  },
-  {
-    descricao: 'Taxa do Mercado',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 99',
-  },
-  {
-    descricao: 'Taxa do Banco',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 49',
-  },
-];
+valor = valor.toLocaleString('pt-BR', { style:'currency', currency: 'BRL' }) 
+// valor = valor.toLocaleString('en-US', { style:'currency', currency: 'USD' }) 
 
-let taxaTotal = 0
-let recebimentoTotal = 0
-transacoes.forEach((item) => {
-  const valorLimpo = +item.valor.replace('R$ ', '')
-  if(item.descricao.slice(0,4) === 'Taxa') {
-    taxaTotal += valorLimpo
-  } else if (item.descricao.slice(0,4) === 'Rece') {
-    recebimentoTotal += valorLimpo
-  }
+// 'pt-BR' = Formato do Brasil
+// style:'currency' = para moeda
+// currency: 'BRL' = O simbolo usado R$
+
+console.log(valor)
+
+// const aleatorio = Math.floor(Math.random() * 100)
+// const aleatorio = Math.floor(Math.random() * (max - min + 1)) + min
+const aleatorio = Math.floor(Math.random() * (500 - 300 + 1)) + 300
+
+console.log(aleatorio)
+
+// ====================== Exercicios ====================
+
+// Retorne um número aleatório
+// entre 1050 e 2000
+
+const numeroAleatorio = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050
+
+console.log(numeroAleatorio)
+
+
+// Retorne o maior número da lista abaixo
+let numeros = '4, 5, 20, 8, 9';
+numeros = numeros.split(', ')
+
+console.log(numeros)
+
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+
+
+let somaTotal = 0
+listaPrecos.forEach((item) => {
+  const numeroLimpo = item.toUpperCase().replace('R$ ', '').trim()
+  const numeroArredondado = Number.parseFloat(numeroLimpo)
+  somaTotal += numeroArredondado
 })
 
-console.log(`Taxa = ${taxaTotal}`)
-console.log(`Recebimento = ${recebimentoTotal}`)
-
-// Retorne uma array com a lista abaixo
-const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-const arrayTransportes = transportes.split(';')
-
-console.log(arrayTransportes)
-
-// Substitua todos os span's por a's
-const html = `<ul>
-                <li><span>Sobre</span></li>
-                <li><span>Produtos</span></li>
-                <li><span>Contato</span></li>
-              </ul>`;
-
-const htmlNovo = html.split('span').join('a')
-
-console.log(htmlNovo)
-
-// Retorne o último caracter da frase
-const frase = 'Melhor do ano!';
-console.log(`Ultimo caracter = '${frase[frase.length - 1]}'`)
-console.log(frase.slice(-1))
-
-// Retorne o total de taxas
-const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-
-let totalTaxas = 0
-transacoes2.forEach((item) => {
-  if(item.trim().toLowerCase().includes('taxa')) {
-    totalTaxas += 1
-  }
-})
-
-console.log(`Existe ${totalTaxas} taxas`)
+console.log(somaTotal)
