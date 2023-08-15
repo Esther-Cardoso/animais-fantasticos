1321ad5453b5f0cbcaecc09a483c242f72566687
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-08-15 09:30:37 -0300

    dataset

--- efeitos-no-dom.js
+++
@@ -1,8 +0,0 @@
-const div = document.querySelector('[data-cor]')
-
-div.dataset.bg = 'red white'
-delete div.dataset.width
-div.dataset.totalWidth = 2000
-
-console.log(div.dataset)
-console.log(div.dataset.cor)
