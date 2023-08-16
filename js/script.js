6589976986a14028de4e2ff6af0391a1cb07892f
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-08-16 10:52:10 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/animacao-scroll.js"
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tabnav.js";
 import initTop from "./modules/topo.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave()
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
 initTop()
+initModal()
