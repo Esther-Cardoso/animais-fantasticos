020719956ee00614fc7a1cd12809cfd25fe97878
Esther Cardoso <esthercardosofernandes@gmail.com>
2024-01-22 15:09:42 -0300

    refatorando scroll suave

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-import initScrollSuave from "./modules/scroll-suave.js"
-import initAnimacaoScroll from "./modules/animacao-scroll.js"
-import initAccordion from "./modules/accordion.js"
-import initTabNav from "./modules/tabnav.js";
-import initTop from "./modules/topo.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initFucionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import ScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initTop from './modules/topo.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFucionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
-initScrollSuave()
-initAnimacaoScroll()
-initAccordion()
-initTabNav()
-initTop()
-initModal()
-initTooltip()
-initDropdownMenu()
-initMenuMobile()
-initFucionamento()
-initFetchAnimais()
-initFetchBitcoin()
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initTop();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFucionamento();
+initFetchAnimais();
+initFetchBitcoin();
