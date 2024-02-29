3a76b4fcf17cbbaefe4e00080fc0f88ecd8e4d33
Esther Cardoso <esthercardosofernandes@gmail.com>
2024-02-29 10:34:10 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import initTop from './modules/topo.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFucionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
@@ -39,8 +39,10 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initTop();
-initMenuMobile();
 initFucionamento();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
